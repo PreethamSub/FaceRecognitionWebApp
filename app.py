1589db635f535f00a3f97935ff4c34364632e417
PreethamSub <91767887+PreethamSub@users.noreply.github.com>
2022-11-12 21:01:22 +0530

    git auto update

--- app.py
+++ app.py
@@ -18,6 +18,7 @@
 
 import face_recognition
 from flask import Flask, jsonify, request, redirect
+import git
 
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -29,6 +30,14 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo("./FaceRecognitionWebApp")
+    origin = repo.remote.origin
+    repo.create_head('main',origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull
+    return '',200
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
